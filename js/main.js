4e3e5a74998f8c3d39d8a27af5bbde7e04e707be
b-hans <b-hans@umn.edu>
2020-12-01 11:26:24 -0600

    added jquery functionality for next

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	$('.next').on('click', function(){
-		console.log('clicked');
+		var currentImg = $('.active');
+		var nextImg = currentImg.next();
+		
+		if (nextImg.length){
+			currentImg.removeClass('active').css('z-index', -10);
+			nextImg.addClass('active').css('z-index', 10);
+		}
 	});
 });
